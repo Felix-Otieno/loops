e415933e5e6be105b1614c385eb64fc5d1494b57
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-07-15 15:25:53 +0300

    about loops types

---
+++ dowhileloop.js
@@ -0,0 +1,17 @@
+let a = 0;
+
+do {
+    if (a%2 === 0) 
+        {console.log ("these are even numbers in do..while loop:" + a)
+    
+}
+a++ 
+}
+while (a <= 20)
+    
+
+
+   
+    
+
+
---
+++ whileloop.js
@@ -0,0 +1,8 @@
+let a = 0;
+while(a <= 20) {
+
+    if (a % 2 === 0) {
+        console.log("These are even numbers in while loop:"+ a);
+    }
+ a++   
+}
